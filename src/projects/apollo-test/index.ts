a93943e51328afa689042fbebd8034cb34647411
kolian <ntrof@icloud.com>
2021-09-15 16:24:54 +0300

    db-migrate-undo

--- src/projects/apollo-test/index.ts
+++ src/projects/apollo-test/index.ts
@@ -3,32 +3,69 @@ RunTimeConfig.set('apollo-test');
 import { ApolloServer, gql } from 'apollo-server-koa';
 import { ApolloServerPluginInlineTrace } from 'apollo-server-core';
 import Koa from 'koa';
+import { Sequelize as Seq } from 'sequelize-typescript';
+import { Sequelize, Op } from 'sequelize';
+import { Author, Book } from './sequelize/models';
+import { recursiveReplace } from '~l/util';
+
+const seq = new Seq(RunTimeConfig.get().dbConfig);
+seq.addModels([Author, Book]);
 
 const typeDefs = gql`
+  type Author {
+    id: Int
+    firstName: String
+    lastName: String
+    middleName: String
+    birthDate: Int
+  }
   type Book {
-    title: String
-    author: String
+    id: Int
+    name: String
+    publicationYear: Int
+    author: Author
   }
   type Query {
-    booksByAuthors(author: String): [Book]
-    booksByTitles(title: String): [Book]
+    findBooks(queryString: String): [Book]
   }
 `;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
 const resolvers = {
   Query: {
-    booksByAuthors: (_parent, args) => books.filter((item) => item.author === args.author),
-    booksByTitles: (_parent, args) => books.filter((item) => item.title === args.title),
+    findBooks: async (_parent, args) => {
+      const queryString = recursiveReplace(args.queryString, '  ', '').toLowerCase();
+      if (queryString.replace(' ', '').length < 3) {
+        return [];
+      }
+      const arr = queryString.split(' ');
+      const authors = await Author.findAll({
+        where: Sequelize.where(
+          Sequelize.fn(
+            'lower',
+            Sequelize.fn('concat', Sequelize.col('firstName'), ' ', Sequelize.col('lastName'))
+          ),
+          {
+            [Op.like]: {
+              [Op.any]: [`%${queryString}%`, `%${arr[1] + ' ' + arr[0]}%`],
+            },
+          }
+        ),
+      });
+
+      const where = authors.length
+        ? { authorId: { [Op.in]: authors.map((item) => item.id) } }
+        : Sequelize.where(Sequelize.fn('lower', Sequelize.col('name')), {
+            [Op.like]: `%${queryString}%`,
+          });
+      return Book.findAll({
+        attributes: ['id', 'name', 'publicationYear'],
+        include: {
+          model: Author,
+          required: true,
+          attributes: ['id', 'firstName', 'lastName', 'middleName', 'birthDate'],
+        },
+        where,
+      });
+    },
   },
 };
 const startApolloServer = async (typeDefs, resolvers) => {
--- src/projects/koa-test/index.ts
+++ src/projects/koa-test/index.ts
@@ -14,35 +14,40 @@ const router = new Router();
 router.get('/findBooks', async (ctx) => {
   ctx.status = 200;
   let { queryString } = ctx.query as { queryString: string };
+  ctx.response.body = [];
+  if (queryString.replace(' ', '').length < 3) {
+    return;
+  }
   queryString = recursiveReplace(queryString, '  ', '').toLowerCase();
   const arr = queryString.split(' ');
-  const where = Sequelize.where(
-    Sequelize.fn(
-      'lower',
-      Sequelize.fn('concat', Sequelize.col('firstName'), ' ', Sequelize.col('lastName'))
+  const authors = await Author.findAll({
+    where: Sequelize.where(
+      Sequelize.fn(
+        'lower',
+        Sequelize.fn('concat', Sequelize.col('firstName'), ' ', Sequelize.col('lastName'))
+      ),
+      {
+        [Op.like]: {
+          [Op.any]: [`%${queryString}%`, `%${arr[1] + ' ' + arr[0]}%`],
+        },
+      }
     ),
-    {
-      [Op.like]: {
-        [Op.any]: [`%${queryString}%`, `%${arr[1] + ' ' + arr[0]}%`],
-      },
-    }
-  );
-  const authors = await Author.findAll({ where });
-  let books;
-  if (authors.length) {
-    books = await Book.findAll({
-      include: { model: Author, required: true },
-      where: { authorId: { [Op.in]: authors.map((item) => item.id) } },
-    });
-  } else {
-    books = await Book.findAll({
-      include: { model: Author, required: true },
-      where: Sequelize.where(Sequelize.fn('lower', Sequelize.col('name')), {
+  });
+
+  const where = authors.length
+    ? { authorId: { [Op.in]: authors.map((item) => item.id) } }
+    : Sequelize.where(Sequelize.fn('lower', Sequelize.col('name')), {
         [Op.like]: `%${queryString}%`,
-      }),
-    });
-  }
-  ctx.response.body = books;
+      });
+  ctx.response.body = await Book.findAll({
+    attributes: ['id', 'name', 'publicationYear'],
+    include: {
+      model: Author,
+      required: true,
+      attributes: ['id', 'firstName', 'lastName', 'middleName', 'birthDate'],
+    },
+    where,
+  });
 });
 
 server(router).then();
